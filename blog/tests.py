6c3dbecacb076c7d6502c15d30b600b9ed846b2a
Aaditya karna <aadityakarna4321@gmail.com>
2026-05-05 19:28:14 +0545

    Fix tests for Profile signal + IntegrityError import

--- blog/tests.py
+++ blog/tests.py
@@ -1,7 +1,7 @@
 from django.test import TestCase
 from django.contrib.auth.models import User
 from django.urls import reverse
-from django import IntegrityError
+from django.db.utils import IntegrityError
 
 from .models import Post, Comment, Like
 
--- users/tests.py
+++ users/tests.py
@@ -1,11 +1,17 @@
 from django.test import TestCase
 from django.contrib.auth.models import User
-
 from .models import Profile
 
 
-class ProfileModelTests(TestCase):
-    def test_profile_str(self):
+class ProfileSignalTests(TestCase):
+    def test_profile_is_created_for_new_user(self):
         user = User.objects.create_user(username="david", password="pass12345")
-        profile = Profile.objects.create(user=user, bio="hi")
+
+        # signal should have created it automatically
+        self.assertTrue(Profile.objects.filter(user=user).exists())
+        profile = Profile.objects.get(user=user)
         self.assertEqual(str(profile), "david Profile")
+
+    def test_only_one_profile_per_user(self):
+        user = User.objects.create_user(username="eva", password="pass12345")
+        self.assertEqual(Profile.objects.filter(user=user).count(), 1)
